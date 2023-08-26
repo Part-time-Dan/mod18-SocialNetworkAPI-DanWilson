ec5ace32a753a3dfdc6ec57dc28927e9e6acc674
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-08-26 16:37:31 -0600

    set up server routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController');
 
-
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
